3c22ed32e720841d83ff0616640af129865ff233
Humilitas <26362027+Humilitas@users.noreply.github.com>
2021-06-28 16:52:50 +0800

    Add files via upload

---
+++ src/Redux/reducers/index.js
@@ -0,0 +1,9 @@
+import counterReducer from "./counterReducer";
+import authReducer from "./authReducer";
+import {combineReducers} from "redux";
+
+const allReducers = combineReducers({
+    counter: counterReducer,
+    auth: authReducer,
+});
+export default allReducers;
